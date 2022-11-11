785fea7d7282a2a70b43463d25ef932acf7d5979
AidenValley <jhyjsh953@gmail.com>
2022-11-11 13:44:29 -0800

    resume add in about

--- src/components/About.js
+++ src/components/About.js
@@ -3,28 +3,42 @@ import { Icon } from "@iconify/react";
 import nodejs from "@iconify/icons-logos/nodejs";
 import reactIcon from "@iconify/icons-logos/react";
 import postgresql from "@iconify/icons-logos/postgresql";
-
-
+import documentPdf from "@iconify/icons-carbon/document-pdf";
 
 class About extends Component {
   render() {
-      const sectionName = ['About Me'];
-      const hello = ['Hello to Everyone!'];
-      const about = ["I develop abstract ideas and turn them into cohesive designs, delivering the best scalable, stable, and high-quality platform.", 
+    const sectionName = ["About Me"];
+    const hello = ["Hello to Everyone!"];
+    const about = [
+      "I develop abstract ideas and turn them into cohesive designs, delivering the best scalable, stable, and high-quality platform.",
       "I am a former Customs Entry Writer and Digital Marketer that chose to pursue my passion. General Assembly provided me with a full spectrum of skills and coaching that will allow me to effectively develop a world-class user experience.",
-      "If you are seeking a top-rated web developer, please Git to know me." ];
-     
+      "If you are seeking a top-rated web developer, please Git to know me.",
+    ];
+
     return (
       <section id="about">
         <div className="col-md-12">
           <h1 style={{ color: "black" }}>
             <span>{sectionName}</span>
+            <span>
+              <a href="https://drive.google.com/file/d/1X1jAoTQt7wz8VjAfg66IWKhihaRvKMAw/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <Icon
+                  icon={documentPdf}
+                  width="30"
+                  height="30"
+                  className="ml-4 cursor:wait"
+                  />
+              </a>
+            </span>
           </h1>
           <div className="row center mx-auto mb-5">
             <div className="col-md-4 mb-5 center">
               <div className="polaroid">
                 <span style={{ cursor: "auto" }}>
-                  <img height="250px" src={ require('./../images/myProfile.jpg')} />
+                  <img
+                    height="250px"
+                    src={require("./../images/myProfile.jpg")}
+                  />
                   <Icon
                     icon={nodejs}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
